f041f63a59f1339fe683e576f595c99b0ae856dc
monicakrystal <monicakrystal@icloud.com>
2024-02-10 14:41:48 -0800

    changes

--- main.py
+++ main.py
@@ -40,3 +40,20 @@ elif weekday == False and vacation == True:
 elif weekday == True and vacation == True:
   return True;
 '''
+
+
+# Given an int n, return the absolute difference between n and 21,
+# except return double the absolute difference if n is over 21.
+
+# diff21(19) → 2
+# diff21(10) → 11
+# diff21(21) → 0
+
+def diff21(n):
+    if n > 21:
+        return (n - 21) * 2
+    elif n < 21:
+        return 21 - n
+
+n = 1
+result = console.log(diff21(n))

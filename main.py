c57a63a6bc71c22a0904b3e06ef954841a7f369e
monicakrystal <monicakrystal@icloud.com>
2024-02-11 20:28:28 -0800

    new challenge added

--- main.py
+++ main.py
@@ -57,3 +57,21 @@ def diff21(n):
 
 result = diff21(23)
 print(result)
+
+
+#   Given 2 ints, a and b, return True if
+#   one of them is 10 or if their sum is 10.
+
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+
+def makes10(a, b):
+  if (a == 10 or b == 10) or (a + b == 10):
+    return True;
+  return False;
+
+a = 5
+b = 5
+
+print(makes10(a, b))
